32dca2a0d003752fff80dc5d7462f3dc80fdd2d5
xlc520 <56480807+xlc520@users.noreply.github.com>
2021-02-16 11:02:23 +0800

    更新客户端版本

--- C189Checkin.py
+++ C189Checkin.py
@@ -8,7 +8,7 @@ password = ""
 
 if(username == "" or password == ""):
     username = input("账号：")
-    print(username[:3] + '****' + username[7:])
+    print(username[:2] + '****' + username[8:])
     password = input("密码：")
 
 def main():
@@ -18,7 +18,7 @@ def main():
     else:
         pass
     rand = str(round(time.time()*1000))
-    surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
+    surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.9.2&model=SM-G930K'
     url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
     url2 = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN_PHOTOS&activityId=ACT_SIGNIN'
     headers = {
